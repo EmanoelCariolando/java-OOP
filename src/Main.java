d4040c72fab6a69a2d3d7f130879f9ad5718b454
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-08 10:09:29 -0300

    sum of triangles without OOP

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        double xA,xB,xC;
+        double yA,yB,yC;
+
+        System.out.println("Enter the measures of triangle X:");
+        xA = sc.nextDouble();
+        xB = sc.nextDouble();
+        xC = sc.nextDouble();
+
+        System.out.println("Enter the measures of triangle Y:");
+        yA = sc.nextDouble();
+        yB = sc.nextDouble();
+        yC = sc.nextDouble();
+
+        double p = (xA + xB + xC) / 2 ;
+        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+        p = (yA + yB + yC) / 2 ;
+        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+        System.out.printf("Triangle X area : %.4f%n", areaX);
+        System.out.printf("Triangle Y area : %.4f%n", areaY);
+
+        if (areaX > areaY) {
+            System.out.printf("Triangle X is the Higher : %.4f%n", areaX);
+        }
+        else {
+            System.out.printf("Triangle Y is the Higher : %.4f%n", areaY);
+        }
+
+        sc.close();
 
         }
     }
