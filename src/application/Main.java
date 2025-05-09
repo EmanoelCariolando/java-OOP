ec05740ecbf1b7fbc71086f0fd636660451432ac
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-09 10:39:34 -0300

    Exercise OOP

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,6 @@
 package application;
 
-import entities.Products;
+import entities.otherClass;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -11,29 +11,25 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Products product = new Products();
 
-        System.out.println("Enter product data");
+        otherClass other = new otherClass();
+
         System.out.println("Name:");
-        product.name = sc.next();
+        other.name = sc.nextLine();
 
-        System.out.println("Price:");
-        product.price = sc.nextDouble();
+        System.out.println("Gross salary:");
+        other.grossSalary = sc.nextDouble();
 
-        System.out.println("Quantity:");
-        product.quantity = sc.nextInt();
+        System.out.println("Tax:");
+        other.tax = sc.nextDouble();
 
-        System.out.println(product);
+        System.out.println("Employee:" + other);
 
-        System.out.println("Enter the number of products to be added in stock:");
-        product.addProducts(sc.nextInt());
+        System.out.println("Which percentage to increase salary?:");
+        other.increaseSalary(sc.nextDouble());
 
-        System.out.println(product);
+        System.out.println("Update data:" + other);
 
-        System.out.println("Enter the number of products to be removed from stock:");
-        product.removeProducts(sc.nextInt());
-
-        System.out.println(product);
 
 
         sc.close();
