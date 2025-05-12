46ed367d4fea8ec60689337bef999aa2c14f6d22
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-12 14:31:29 -0300

    encapsulation, get and set

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,48 @@
 package application;
-import entities.otherClass;
+
+import entities.Products;
 
 import java.util.Locale;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("What is the dollar price?");
-        double dollar = sc.nextDouble();
-        System.out.println("how many dollars will be bought?");
-        double howMany = sc.nextDouble();
-        otherClass.dollarCost(dollar,howMany);
+        System.out.println("Enter product data");
+        System.out.println("Name:");
+        String name = sc.next();
 
-        System.out.printf("Amount to be paid in real: %.2f", otherClass.sumIof(dollar,howMany));
+        System.out.println("Price:");
+        double price = sc.nextDouble();
+
+        System.out.println("Quantity:");
+        int quantity = sc.nextInt();
+
+
+        Products product = new Products(name,price,quantity);
+        product.setName("Computer");
+        product.setPrice(902.00);
+
+        System.out.println("New product:" + product.getName() + " , " + "new price:" + product.getPrice());
+
+        System.out.println(product);
+
+        System.out.println("Enter the number of products to be added in stock:");
+        product.addProducts(sc.nextInt());
+
+        System.out.println(product);
+
+
+
+        System.out.println("Enter the number of products to be removed from stock:");
+        product.removeProducts(sc.nextInt());
+
+        System.out.println(product);
+
 
         sc.close();
 
-        }
-
     }
+}
