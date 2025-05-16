e8906e6fa0b0428cb749a1041a4bff02f988e0e5
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-15 21:11:09 -0300

    For-each

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,38 +10,16 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.print(" How Many Numbers Will You Type? ");
-        int n = sc.nextInt();
 
+        String[] names = new String[]{"Maria","Macaco","Marcos"};
 
-        Persons[] p = new Persons[10];
-
-
-        for (int i = 0; i < n; i++) {
-            System.out.print(" Name: ");
-            String names = sc.next();
-            System.out.print(" Email: ");
-            String email = sc.next();
-
-            int room;
-            while(true) {
-                System.out.print(" Room: ");
-                room = sc.nextInt();
-                if (room < 10 && p[room] == null) {
-                    break;
-                } else {
-                    System.out.println("room rented or unavailable ");
-                }
-            }
-            p[room] = new Persons(names,email,room);
+        for (String allName : names){
+            System.out.println(allName);
         }
 
-            System.out.println("Busy Room");
-            System.out.println(p);
-            for ( int i = 0; i < p.length; i++) {
-                if (p[i] != null) {
-                    System.out.println(p[i]); // chama automaticamente o toString()
-                }
-              }
-            }
-         }
+
+    }
+}
+
+
+
