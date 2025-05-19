fda672710f2bf3ca30f1598904ffa213b4703536
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-19 20:50:14 -0300

    Enums

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,57 +1,22 @@
 package application;
 
-import entities.Persons;
+import entities.enums.OrderStatus;
+import mainEntities.Order;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-import java.util.Scanner;
-
+import java.util.Date;
 
 public class Main {
-
         public static void main(String[] args) {
 
-            DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+                Order order = new Order(10, new Date(), OrderStatus.PENDING);
+                System.out.println(order);
 
-            LocalDate d01 = LocalDate.now();
-            LocalDateTime d02 = LocalDateTime.now();
-            Instant d03 = Instant.now();
-
-            LocalDate dp01 = LocalDate.parse("2025-10-15");
-            LocalDateTime dp02 = LocalDateTime.parse("2025-10-15T10:15:30");
-            Instant dp03 = Instant.parse("2025-10-15T01:15:30Z");
-
-             LocalDate dpf01 = LocalDate.parse("15/10/2005", fmt1);
-            LocalDateTime dpf02 = LocalDateTime.parse("15/10/2005 10:15", fmt2);
-
-
-            System.out.println("d01 = " + d01);
-            System.out.println("d02 = " + d02);
-            System.out.println("d03 = " + d03);
-
-            System.out.println();
-
-            System.out.println("dp1 = " + dp01);
-            System.out.println("dp2 = " + dp02);
-
-            System.out.println();
-
-            System.out.println("dpf1 = " + dpf01.toString());
-            System.out.println("dpf2 = " + dpf02.toString());
-
-
-
-
-
-
-
-
-
-
+                OrderStatus x1 = OrderStatus.PENDING;
+                OrderStatus x2 = OrderStatus.valueOf("PENDING");
+                
+                System.out.println(x1 == x2);
+                System.out.println(x1);
+                System.out.println(x2);
 
         }
 }
