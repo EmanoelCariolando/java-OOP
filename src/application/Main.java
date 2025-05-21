cd4647b7172773a07ca57db313f7e8b2e8523322
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-21 15:09:58 -0300

    Project Enumeration

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,66 @@
 package application;
 
-import entities.enums.OrderStatus;
-import mainEntities.Order;
 
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entitiesEnum.WorkerLevel;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Month;
 import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
+        public static void main(String[] args) throws ParseException {
+                Locale.setDefault(Locale.US);
+                Scanner sc = new Scanner(System.in);
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-                Order order = new Order(10, new Date(), OrderStatus.PENDING);
-                System.out.println(order);
+                System.out.println("Enter the department's name: ");
+                String department = sc.next();
+                System.out.println("Enter the worker's data: ");
+                System.out.print("Name: ");
+                String name = sc.next();
+                System.out.print("Level: ");
+                String level = sc.next();
+                System.out.print("Base Salary: ");
+                Double baseSalary = sc.nextDouble();
 
-                OrderStatus x1 = OrderStatus.PENDING;
-                OrderStatus x2 = OrderStatus.valueOf("PENDING");
-                
-                System.out.println(x1 == x2);
-                System.out.println(x1);
-                System.out.println(x2);
+                Worker worker = new Worker(name,WorkerLevel.valueOf(level),baseSalary,new Department(department));
+
+                System.out.print("Number of Contracts: ");
+                Integer numberOfContracts = sc.nextInt();
+
+
+                for (int i = 0; i < numberOfContracts; i++) {
+                        System.out.println("Enter contract #" + (i + 1) + " data: ");
+                        System.out.print("Date (dd/MM/yyyy): ");
+                        Date dates = sdf.parse(sc.next());
+                        System.out.print("Value per hour: ");
+                        Double valuePerHour = sc.nextDouble();
+                        System.out.print("Duration(hours): ");
+                        Integer hours = sc.nextInt();
+
+                        HourContract con = new HourContract(dates,valuePerHour,hours);
+                        worker.addContract(con);
+                }
+
+                System.out.println("Enter month and year to calculate salary: (MM/YYYY)");
+                String monthYear = sc.next();
+                int month = Integer.parseInt(monthYear.substring(0,2));
+                int year = Integer.parseInt(monthYear.substring(3));
+                double sums = worker.income(year,month);
+
+                System.out.println("Name: " + worker.getName());
+                System.out.println("Department: " + worker.getDepartment());
+
+                System.out.printf("Income for %d/%d: $ %.2f%n",month,year,sums);
+
+                sc.close();
+
 
         }
 }
--- src/entities/enums/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package entities.enums;
-
-public enum OrderStatus {
-    PENDING,
-    PROCESSING,
-    SHIPPED,
-    DELIVERED;
-}
